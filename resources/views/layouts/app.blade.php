50c2a23d3a1dce411124f262be251794e11fe580
guanyinhou <moonightriver@gmail.com>
2020-12-26 18:52:09 +0800

    laravel+vue-crud-14

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,21 +10,21 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios/min.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js'></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
@@ -42,7 +42,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item active">
+                            <a href="{{ route('persons.index') }}" class="nav-link">聯絡人</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a href="#" class="nav-link">@{{ message }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -85,5 +90,8 @@
             @yield('content')
         </main>
     </div>
+    <script>
+        @yield('script')
+    </script>
 </body>
 </html>
--- resources/views/persons/index.blade.php
+++ resources/views/persons/index.blade.php
@@ -1,1 +1,132 @@
-This is persons index blade.
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header">聯絡人</div>
+
+        <div class="card-body">
+          <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            @{{ message }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+
+          <button class="btn btn-primary btn-sm mb-2" type="button" @click="create()">新增</button>
+
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">姓名</th>
+                <th scope="col">地址</th>
+                <th scope="col">生日</th>
+                <th scope="col">手機</th>
+                <th scope="col">電郵</th>
+                <th scope="col">操作</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="person in persons" :key="person.id">
+                <td class="text-nowrap">@{{ person.name }}</td>
+                <td class="text-nowrap">@{{ person.address }}</td>
+                <td class="text-nowrap">@{{ person.birthday }}</td>
+                <td class="text-nowrap">@{{ person.mobile }}</td>
+                <td class="text-nowrap">@{{ person.email }}</td>
+                <td class="text-nowrap">
+                  <button class="btn btn-success btn-sm" type="button" data-toggle="modal" data-target="#createForm">編輯</button>
+                  <button class="btn btn-danger btn-sm" type="button">刪除</button>
+                </td>
+              </tr>
+            </tbody>
+            <tfoot v-if="persons.length == 0">
+              <tr>
+                <td colspan="100">無資料</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="createForm" tabindex="-1" aria-labelledby="createFormLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="createFormLabel">新增聯絡人</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form id="createForm">
+        <!-- 編輯用的隱藏欄位 -->
+        <input type="text" name="id">
+        <div class="form-group">
+          <label for="name">姓名</label>
+          <input type="text" class="form-control" id="name" name="name">
+        </div>
+        <div class="form-group">
+          <label for="address">地址</label>
+          <input type="text" class="form-control" id="address" name="address">
+        </div>
+        <div class="form-group">
+          <label for="birthday">生日</label>
+          <input type="date" class="form-control" id="birthday" name="birthday">
+        </div>
+        <div class="form-group">
+          <label for="mobile">手機</label>
+          <input type="text" class="form-control" id="mobile" name="mobile">
+        </div>
+        <div class="form-group">
+          <label for="email">電郵</label>
+          <input type="email" class="form-control" id="email" name="email">
+        </div>
+      </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+        <button type="button" class="btn btn-primary">儲存</button>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+
+@section('script')
+
+  'use strict'
+
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello vue !',
+      persons: []
+    },
+    created: function(){
+      const url = "{{ route('api.persons') }}";
+      console.log(url);
+
+      axios.get(url)
+        .then(function(res){
+          console.log(res);
+          app.persons = res.data;
+        });
+
+      console.log('vue created');
+    },
+    methods: {
+      create: function(){
+        $("#createForm").find('input').val("");
+        $("#createForm").modal('show');
+        console.log('persons.create');
+      }
+    }
+  })
+
+@endsection
